19fb16bfa0be0efac1e90c4f20c544995cb6e37a
Shivang Sharma <sharmashivang1999@gmail.com>
2024-06-09 16:26:14 +0530

    feat: add the signin comp to header

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,53 +1,17 @@
-import { useDispatch, useSelector } from "react-redux";
-import { useState, useEffect } from "react";
-import { YOUTUBE_SEARCH_API } from "../utils/constants";
+import { useDispatch } from "react-redux";
 import {
   HAMBURGER_LOGO_URL,
   USER_ICON_IMG,
   YOUTUBE_LOGO_URL,
 } from "../utils/constants";
 import { toggleMenu } from "../redux/slices/appSlice";
-import { cacheResults } from "../redux/slices/searchSlice";
+import SignInButton from "./SignInButton";
+import SearchContainer from "./SearchContainer";
 
 const Head = () => {
   const dispatch = useDispatch();
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
-  };
-
-  const [searchQuery, setSearchQuery] = useState("");
-  const [suggestions, setSuggestions] = useState([]);
-  const [showSuggestions, setShowSuggestions] = useState(false);
-
-  const searchCache = useSelector((store) => store.search);
-
-  useEffect(() => {
-    // make an API call after every key press
-    // if the time difference between 2 API calls is < 200ms
-    // decine the API call
-
-    const searchTimer = setTimeout(() => {
-      if (searchCache[searchQuery]) setSuggestions(searchCache[searchQuery]);
-      else getSearchSuggestions();
-    }, 200);
-
-    return () => {
-      // this clear timeout kills the current timer each time the component rerenders
-      clearTimeout(searchTimer);
-    };
-  }, [searchQuery]);
-
-  const getSearchSuggestions = async () => {
-    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
-    const jsonData = await data.json();
-
-    setSuggestions(jsonData[1]);
-
-    dispatch(
-      cacheResults({
-        [searchQuery]: jsonData[1],
-      })
-    );
   };
 
   return (
@@ -63,38 +27,11 @@ const Head = () => {
           <img alt="youtube-logo" src={YOUTUBE_LOGO_URL} className="h-8 mx-4" />
         </a>
       </div>
-      <div className="col-span-10 px-10">
-        <div className="grid grid-flow-col">
-          <input
-            type="text"
-            className="col-span-9 border-2 border-gray-400 rounded-l-full px-7"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            onFocus={() => setShowSuggestions(true)}
-            onBlur={() => setShowSuggestions(false)}
-            onScroll={() => setShowSuggestions(false)}
-          />
-          <button className="col-span-3 border-2 border-gray-400 p-2 w-16 rounded-r-full">
-            Search
-          </button>
-        </div>
-        {showSuggestions && (
-          <div className="absolute z-10 bg-white px-5 py-2 w-[43rem] shadow-lg rounded-lg border border-gray-100">
-            <ul>
-              {suggestions.map((suggestion) => {
-                return (
-                  <li key={suggestion} className="py-2 px-2 hover:bg-gray-200">
-                    {suggestion}
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )}
-      </div>
+      <SearchContainer />
       <div className="col-span-1">
         <img alt="user-icon" src={USER_ICON_IMG} className="h-8" />
       </div>
+      <SignInButton />
     </div>
   );
 };
---
+++ src/components/SignInButton.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+// import { useNavigate } from "react-router-dom";
+
+const SignInButton = () => {
+  //   const navigate = useNavigate();
+
+  const handleSignIn = () => {
+    // navigate to login route
+  };
+
+  return (
+    <button
+      onClick={handleSignIn}
+      className="flex items-center px-4 py-2 bg-white text-blue-600 border border-gray-300 rounded-full shadow hover:bg-gray-100 transition duration-150 ease-in-out"
+    >
+      <svg
+        className="w-5 h-5 mr-2"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20ZM13 13H11V17H13V13ZM13 7H11V11H13V7Z"
+          fill="currentColor"
+        />
+      </svg>
+      Sign In
+    </button>
+  );
+};
+
+export default SignInButton;
